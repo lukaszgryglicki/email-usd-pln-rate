61d3a129ff2faa8cf072e5f43c80a302d8fd8140
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-03-12 16:15:22 +0000

    Monitor USD rate
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # email-usd-pln-rate
+
 Get USD/PLN walutomat.pl exchange rate using phantomjs/pup and send periodic email using cronjob/sendmail
+
+
+# Requirements
+
+- Linux (other OSes may work too, never tested).
+- [phantomjs](https://gist.github.com/julionc/7476620) - headless [version](https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2) not requiring X server.
+- [pup](https://github.com/ericchiang/pup): `go get github.com/ericchiang/pup`.
+- Installed & configured `sendmail`.
+
+# Run
+
+- `./get_rate.sh`.
+
+# Cronjob
+
+- `cp get_rate.sh /usr/local/bin`.
+- Copy `get_rate.crontab` file then edit crontab via `crontab -e` and paste contents.
+
---
+++ get_rate.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+read -r -d '' source << EOM
+var system = require('system');
+var page = require('webpage').create();
+
+var url = system.args[1];
+var destination = system.args[2];
+
+page.settings.resourceTimeout = 1000;
+
+setTimeout(function(){
+    setInterval(function () {
+var fs = require('fs');
+var page = require('webpage').create();
+page.open(url, function () {
+    console.log(page.content);
+try {
+    fs.write(destination, page.content, 'w');
+    } catch(e) {
+        console.log(e);
+    }
+    phantom.exit();
+});
+    }, 2000);
+}, 1);
+EOM
+echo $source > save.js
+function finish {
+    rm -f save.js page.html temp.txt 2>/dev/null
+}
+trap finish EXIT
+export PATH="${PATH}:/home/justa/dev/go/bin:/usr/local/bin:/usr/bin:/usr/sbin:/bin"
+rate=`phantomjs save.js https://www.walutomat.pl page.html 1>/dev/null 2>/dev/null && cat page.html | pup '.USD_PLN .buy' | head -n 2 | tail -n 1 | awk '{$1=$1};1'`
+if [ -z "${rate}" ]
+then
+  echo "$0: error getting USD?PLN rate"
+  exit 1
+fi
+MAIL_TO=lukaszgryglicki@o2.pl
+echo "USD/PLN: ${rate}"
+host=`hostname`
+echo "From: usd_pln_rate_$$@${host}" > temp.txt
+echo "To: ${MAIL_TO}" >> temp.txt
+echo "Subject: USD/PLN rate ${rate}" >> temp.txt
+echo '' >> temp.txt
+echo "USD/PLN: ${rate}" >> temp.txt
+sendmail $MAIL_TO < temp.txt
